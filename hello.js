20d2e89d55dfa0ff908a05c964ed502857db472b
Anit <anittankandasamy@gmail.com>
2022-08-17 22:22:23 +0000

    Created variable function that returns a value instead of consoles it. Will now have to console.log outside the function

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
-const sayHello = function (name) {
-  console.log("Hello, " + name);
+const returnSayHello = function (name) {
+  return "Hello, " + name;
 }
 
-sayHello("Caliban");
+const greeting = returnSayHello("John");
+console.log(greeting);
