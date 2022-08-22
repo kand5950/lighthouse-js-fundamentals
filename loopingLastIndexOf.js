7ceab72943e62ce3bdb0a867c938c2ce00dc7644
Anit <anittankandasamy@gmail.com>
2022-08-22 02:39:49 +0000

    created a function that found last index using for loop and if statement

---
+++ loopingLastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = function (array, value){
+  for (let i = array.length - 1; i >= 0; i--){
+    if (array[i] === value){
+    return i;
+    }
+    }
+      return -1;
+    }
+    
+  
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1));
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2));
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3));
+console.log(lastIndexOf([ 5, 5, 5 ], 5));
+console.log(lastIndexOf([], 3));
