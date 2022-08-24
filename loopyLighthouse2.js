3c276ea9c8fe9563a81d36d16ab6fd0cbc22b34c
Anit <anittankandasamy@gmail.com>
2022-08-24 04:14:28 +0000

    created function with 3 variables range[a,b], multiples[m1,m2], words[word1,word2]. Function starts from range [a] ends with range[b], when divisable by m1 prints word1, m2 prints word2, m1 & m2 prints word1+word2

--- loopyLighthouse2.js
+++ loopyLighthouse2.js
@@ -1,9 +1,20 @@
 const loopyLighthouse = function(range, multiples, words){
   for (let i = range[0] ; i <= range[1] ; i++){
-     console.log(loopyLighthouse(i));
+    if ((i % multiples[0] === 0) && (i % multiples[1] === 0)){
+      console.log("BattyBeacon");
+    }
+    
+    else if (i % multiples[0] === 0){
+      console.log("Batty");
+    }
+    else if (i % multiples[1] === 0){
+        console.log("Beacon");
+      }
+    else console.log(i);
+    }
   }
  
-}
 
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
     
     
