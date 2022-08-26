a8c1d37d6aa7ab8677a9ff31cd41173835dab712
Anit <anittankandasamy@gmail.com>
2022-08-26 02:14:29 +0000

    sorted objects in array based of an arguement and printed best submitter

---
+++ objectsRooting4u.js
@@ -0,0 +1,30 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 10
+  }
+]
+
+const metric = 'plumpness'
+
+const judgeVegetable = function (vegetables, metric) {
+    vegetables.sort(function (a, b) {
+      return b[metric] - a[metric];
+    });
+    return vegetables[0].submitter
+  }
+   
+    
+  
+console.log(judgeVegetable(vegetables, metric))
